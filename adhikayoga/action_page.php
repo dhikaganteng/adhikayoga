e28b12bcdfcbd6103d3105193793d940e9623487
dhikaganteng <46327312+dhikaganteng@users.noreply.github.com>
2019-01-03 01:29:52 +0700

    Add files via upload

---
+++ adhikayoga/action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Hai <?php echo $_POST["namadepan"+"namabelakang"]; ?><br>
+Selamat kamu telah menjadi fans KRIWIL FC
+Kamu akan masuk ke KRIWIL FC bagian <?php echo $_POST["domisili"]; ?>
+
+</body>
+</html>
